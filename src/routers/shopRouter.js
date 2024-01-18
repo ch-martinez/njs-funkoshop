aad5b186325dab6dcca691c09a8888d8aab6cfa7
Christian Martinez <chmartinez.cba@gmail.com>
2024-01-17 23:25:02 -0300

    [FEAT] Se añade ruta para filtrar shop por coleccion

--- src/routers/shopRouter.js
+++ src/routers/shopRouter.js
@@ -3,12 +3,7 @@ const router = express.Router()
 const shopController = require('../controllers/shopController')
 
 router.get('/', shopController.shopView)
+router.get('/?c=', shopController.shopView)
 router.get('/item/:id', shopController.productView)
-router.post('/cart', shopController.cartView)
-
-
-router.get('/admin', (req,res) => {
-    res.render('pages/admin/admin.ejs',{ layout: '../views/layouts/adminLayout' })
-})
 
 module.exports = router
