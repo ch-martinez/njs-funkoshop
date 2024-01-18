c2a37fddedc9a7d3beeab5b7955718f9b4c3e3ff
Christian Martinez <chmartinez.cba@gmail.com>
2024-01-17 22:27:32 -0300

    [FEAT] Se añaden validaciones de login

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
+const authValidations = require('../middlewares/authValidations')
 
 router.get('/login', authController.loginView)
+router.post('/login', authValidations.loginValidate, authController.login)
+router.get('/register', authController.registerView)
+router.get('/recover', authController.recoverPassView)
 
 module.exports = router
