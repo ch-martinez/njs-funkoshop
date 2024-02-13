fc4a3b72c1f38bd2a4e6cd2bb1dae83b045d56a3
Christian Martinez <chmartinez.cba@gmail.com>
2024-02-13 16:08:39 -0300

    fix: se corrije nombre de llamada al modelo de coleciones

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,7 +3,7 @@ const productsModels = require('../models/productsModel')
 
 const homeView = async (req, res) => {
     const products = await productsModels.getAllProductsFromBD()
-    const collections = await collectionsModels.getAllCollectionsFromBD()
+    const collections = await collectionsModels.getCollectionsHomeActiveFromDB()
     const page = {
         title: 'FunkoShop',
         glide: true
