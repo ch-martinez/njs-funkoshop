e21f2c633573ec1beac085ac5ea42982978f8626
Christian Martinez <chmartinez.cba@gmail.com>
2024-02-24 18:00:31 -0300

    feat: api para insertar, elimianr y actualizar productos en el carrito

---
+++ src/controllers/apiController.js
@@ -0,0 +1,28 @@
+const cartController = require('../models/cartModel')
+
+
+const addProductToCartInDB = async (req, res) => {
+    cartController.addProductToCartDB(req.body)
+    res.send()
+}
+
+const updateProductInCartDB = async (req, res) => {
+    cartController.updateCartDetailDB(req.body)
+    res.send()
+}
+
+const updateCartDB = async (req, res) => {
+    cartController.updateCartDB(req.body)
+}
+
+const deletProductInCartDB = async(req, res) => {
+    cartController.deletProductInCartDetailDB(req.body.product_id)
+    res.send()
+}
+
+module.exports = {
+    addProductToCartInDB,
+    updateProductInCartDB,
+    updateCartDB,
+    deletProductInCartDB
+}
