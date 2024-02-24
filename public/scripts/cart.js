e612fcf541d702cfaa634328fb2e719649aed386
Christian Martinez <chmartinez.cba@gmail.com>
2024-02-24 17:59:31 -0300

    feat: Se crean funciones para añadir productos al carrito, eliminar productos del carrito. Tanto en localstorage como en la DB

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,6 +1,10 @@
 const form = document.querySelector('#product')
+const cartNavbar = document.querySelector('#cartNavbar')
+const btnDelet = document.querySelectorAll('.cart-item__delet')
 
 /* Funciones auxiliares */
+
+// Inicializa el carrito si no tiene productos
 const initializeCart = () => {
     if (!localStorage.cart){
         let cart = {
@@ -21,54 +25,122 @@ const setQuantityItemsCart = (list) => {
     return list.reduce((acum, item) => acum + item.product_quantity, 0)
 }
 
-const setResumeCart = (cart) => {
+const setSubtotalCart = (list) => {
+    return list.reduce((acum, item) => acum + (item.product_quantity * item.product_price), 0)
 }
 
-const isItemInCart = (cart, id) => {
-    return cart.list.map(product => product.product_id).indexOf(id)
-}
+const setCart = async (cart) => {
+    let cartDB = cart
+        cartDB.resume.subtotal = setSubtotalCart(cart.list),
+        cartDB.resume.total = setSubtotalCart(cart.list)
+        cartDB.items = setQuantityItemsCart(cart.list)
 
-/* Principales */
-const addItemToCart = (id,quantity) => {
-    initializeCart()
-    let cart = JSON.parse(localStorage.cart)
-    const product_index = isItemInCart(cart,id)
-
-    if (product_index === -1) {
-        const product = {product_id: id, product_quantity: quantity}
-        cart.list.push(product)
-    } else {
-        cart.list[product_index].product_quantity += quantity
-    }
-
-    cart.items = setQuantityItemsCart(cart.list)
-    localStorage.setItem('cart',JSON.stringify(cart))
-}
-
-/* BD */
-const sendCartToBD = async () => {
+    console.log(cartDB)
     const options = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(localStorage.cart),
-        credentials: omit,
-    };
-    try {
-        const res = await fetch('/api/cart',options)
-            if (!res.ok){
-                throw new Error('Se produjo un error')
-            }
-    } catch (error) {
-        console.log(error)
+        body: JSON.stringify(cartDB.resume)
+    }
+
+    const res = await fetch('/api/updateCart',options)
+    if(res.ok){
+        cart = cartDB
+    } else {
+        throw new Error('Se produjo un error al actualizar el carrito en DB')
     }
 }
 
+// Devuelve la posicion del producto en el carrito. Si el producto no existe devuelve -1
+const isItemInCart = (cart, id) => {
+    return cart.list.map(product => product.product_id).indexOf(id)
+}
+
+/* Principales */
+const addItemToCart = async (id,quantity,price) => {
+    initializeCart()
+
+    let cart = JSON.parse(localStorage.cart)
+    const product_index = isItemInCart(cart,id)
+    const product = {product_id: id, product_quantity: quantity, product_price: price}
+    // Optiones para fetch
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+    }
+
+    if (product_index === -1) { //Si se carga un producto nuevo
+        const res = await fetch('/api/addProduct',options)
+
+        if (res.ok){
+            cart.list.push(product)
+            setCart(cart)
+            localStorage.setItem('cart',JSON.stringify(cart))
+        } else {
+            throw new Error('Se produjo un error al añadir producto en DB')
+        }
+    } else { //Si se actualiza un producto
+        product.product_quantity = cart.list[product_index].product_quantity + quantity //Actualiza el total de productos
+        options.body = JSON.stringify(product) //Actualiza el body de la peticion fetch
+
+        const res = await fetch('/api/updateProduct',options)
+        if (res.ok){
+            cart.list[product_index].product_quantity = product.product_quantity
+            setCart(cart)
+            localStorage.setItem('cart',JSON.stringify(cart))
+        } else {
+            throw new Error('Se produjo un error al actualizar producto en DB')
+        }
+    }
+}
+
+const deletItemInCart = async (product_id) => {
+    let cart = JSON.parse(localStorage.cart)
+    const product_index = isItemInCart(cart,product_id)
+
+    // Optiones para fetch
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({product_id: product_id})
+    }
+
+    const res = await fetch('/api/deletProduct', options)
+    if (res.ok){
+        cart.list.splice(product_index,1)
+        setCart(cart)
+        localStorage.setItem('cart',JSON.stringify(cart))
+
+        window.location.href = ('/cart') // Redirije al carrito para refrescar los datos
+    } else {
+        throw new Error('Se produjo un error al eliminar producto en DB')
+    }
+}
+
+
 /* Eventos */
-form.onsubmit = (e) => {
+
+cartNavbar.addEventListener('click', () => {
+    console.log('ingreso')
+})
+
+btnDelet.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const product_id = btn.dataset.id
+        deletItemInCart(product_id)
+    })
+});
+
+form.addEventListener('submit', (e) => {
     e.preventDefault()
     let id = Number(e.target.dataset.id)
     let quantity = Number(e.target[1].value)
-    addItemToCart(id, quantity)
-}
+    let price = Number(e.target.dataset.price)
+    addItemToCart(id, quantity, price)
+})
