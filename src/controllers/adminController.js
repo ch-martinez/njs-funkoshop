63d9200bea4efc45af590d8512c2e78b1d23875f
Christian Martinez <chmartinez.cba@gmail.com>
2024-03-01 22:01:37 -0300

    feat: se agregan controladores para seccion de proveedores

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,7 @@
 const usersModels = require('../models/usersModel')
 const collectionsModels = require('../models/collectionsModel')
 const productsModels = require('../models/productsModel')
+const providersModel = require('../models/providersModel')
 
 const adminView = (req, res) => {
     console.log(req.session.user)
@@ -60,6 +61,22 @@ const usersViews = async (req, res) => {
     res.render('pages/admin/usersAdmin', {users, layout: 'layouts/adminLayout'})
 }
 
+/* Providers */
+
+const providersView = async (req, res) => {
+    const providers = await providersModel.getAllProvidersFromDB()
+    res.render('pages/admin/providersAdmin', {providers, layout: 'layouts/adminLayout'})
+}
+
+const providerAddView = async (req,res) => {
+    res.render('pages/admin/add/providerAddView', {layout: 'layouts/adminLayout'})
+}
+
+const providerEditView = async (req, res) => {
+    const [provider] = await providersModel.getProviderFromDB(req.params.id)
+    res.render('pages/admin/edit/providerEditView', {provider, layout: 'layouts/adminLayout'})
+}
+
 module.exports = {
     adminView,
     productsView,
@@ -70,5 +87,8 @@ module.exports = {
     collectionEditView,
     collectionsHomeView,
     collectionsHomeUpdate,
-    usersViews
+    usersViews,
+    providersView,
+    providerAddView,
+    providerEditView
 }
