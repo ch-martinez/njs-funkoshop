875d0a03861a1da9a00da8779d1d994b9a89ff63
Christian Martinez <chmartinez.cba@gmail.com>
2024-04-08 21:02:53 -0300

    feat: se añade funcionalidad de windows.back al boton "cancelar"

--- public/scripts/logout.js
+++ public/scripts/logout.js
@@ -1,5 +1,10 @@
 const logout_btn = document.querySelector('#logout_btn')
+const back_btn = document.querySelector('#back_btn')
 
 logout_btn.addEventListener('click', () => {
     document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+})
+
+back_btn.addEventListener('click', () => {
+    window.history.back()
 })
